93060084307a5906a815983eb9047bdc1487cb0d
Jonas Nunes <suporte@jonasnunes.com.br>
2026-01-19 23:24:50 -0300

    Manipulando informações com o DB e entendendo PDO com fetch e fetchAll com FETCH_ASSOC

--- conectandoDB/index.php
+++ conectandoDB/index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Formulário</title>
 </head>
 <body>
-  <h1>foi</h1>
+  <h1>Envio de dadoss</h1>
+
+  <form action="" method="post">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="id_nome">
+    <label for="email">E-mail</label>
+    <input type="email" name="email" id="id_email">
+    <input type="submit" value="Enviar">
+  </form>
 </body>
 </html>
